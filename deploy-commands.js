89a36f50a98f15bbb8acbcd5135bf5679172ab23
Julius M <juliusm9791@gmail.com>
2023-10-13 16:57:34 -0400

    client

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,10 +1,11 @@
 const { REST, Routes } = require("discord.js");
-const { clientId, guildId } = require("./config.json");
 const fs = require("node:fs");
 const path = require("node:path");
 require("dotenv").config();
 const token = process.env.token;
 const commands = [];
+
+const { clientId, guildId } = JSON.parse(process.env.CLIENT);
 
 module.exports = {
   execute() {
